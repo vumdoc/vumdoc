840cf7435bb7b7a913449cb0ce51fbe9d257cdf1
takuma-ru <kanekotakuma@team-lab.com>
2023-12-27 14:49:05 +0900

    🔧 global style 更新

--- packages/core/src/constants/styles/font.ts
+++ packages/core/src/constants/styles/font.ts
@@ -14,8 +14,89 @@ export const satoshiFont = css`
   @font-face {
     font-family: "Satoshi";
     src: url("../assets/fonts/Satoshi-Variable.ttf") format("truetype");
-    font-weight: 300 900;
+    font-weight: 300 400 500 700 900; /* Light Regular Medium Bold Black   */
     font-display: swap;
     font-style: normal;
+  }
+`;
+
+export const typography = css`
+  h1 {
+    font-size: 2rem;
+    font-weight: 700;
+    line-height: 1.2;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 700;
+    line-height: 1.2;
+  }
+
+  h3 {
+    font-size: 1.25rem;
+    font-weight: 700;
+    line-height: 1.2;
+  }
+
+  .text {
+    font-size: 1rem;
+    font-weight: 400;
+    line-height: 1.5;
+
+    &.-strong {
+      font-weight: 700;
+    }
+  }
+
+  .sub-text {
+    font-size: 0.875rem;
+    font-weight: 400;
+    line-height: 1.5;
+
+    &.-strong {
+      font-weight: 700;
+    }
+  }
+
+  :host,
+  ::slotted(*) {
+    h1 {
+      font-size: 2rem;
+      font-weight: 700;
+      line-height: 1.2;
+    }
+
+    h2 {
+      font-size: 1.5rem;
+      font-weight: 700;
+      line-height: 1.2;
+    }
+
+    h3 {
+      font-size: 1.25rem;
+      font-weight: 700;
+      line-height: 1.2;
+    }
+
+    .text {
+      font-size: 1rem;
+      font-weight: 400;
+      line-height: 1.5;
+
+      &.-strong {
+        font-weight: 700;
+      }
+    }
+
+    .sub-text {
+      font-size: 0.875rem;
+      font-weight: 400;
+      line-height: 1.5;
+
+      &.-strong {
+        font-weight: 700;
+      }
+    }
   }
 `;
