cb5b963c752e26ddf561e005ac84c14475fcb4cd
takuma-ru <kanekotakuma@team-lab.com>
2024-01-05 11:40:40 +0900

    💫 vum-button に クリックアニメーションを追加

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,2 +1,3 @@
+export * from "~/src/components/vum-breadcrumbs/vum-breadcrumbs.ce";
 export * from "~/src/components/vum-button/vum-button.ce";
 export * from "~/src/components/vum-chip/vum-chip.ce";
